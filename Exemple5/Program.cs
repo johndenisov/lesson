8829d56f017a47ce2a69ebd2b4215c5aac08a8ea
johndenisov <johndenisov@gmail.com>
2022-12-03 22:36:51 +0300

    div number result

--- Exemple5/Program.cs
+++ Exemple5/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 10;
+int numderB = 2;
+int result = numberA / numderB;
+Console.WriteLine(result);
