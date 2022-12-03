58773cc372c3ecf1210d15f1352be49cc27c4974
johndenisov <johndenisov@gmail.com>
2022-12-03 23:09:27 +0300

    sum rendom  numbers

--- Exemple5/Program.cs
+++ Exemple5/Program.cs
@@ -1,4 +1,7 @@
-﻿double numberA = 11;
-double numderB = 2;
-double result = numberA / numderB;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numderB = new Random().Next(1,10);
+Console.WriteLine(numderB);
+int result = numberA + numderB;
 Console.WriteLine(result);
+
