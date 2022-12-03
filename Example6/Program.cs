e1a6168ea9af0999bc4971874dc05e7271a79fae
johndenisov <johndenisov@gmail.com>
2022-12-03 23:40:03 +0300

    done  project if

--- Example6/Program.cs
+++ Example6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Конструкция ветвления (if)
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower () == "маша")
+{
+    Console.WriteLine("Ура, это Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
