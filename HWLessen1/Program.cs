24465f6ec340aa7370ec066c93f41ce0b928edad
johndenisov <johndenisov@gmail.com>
2022-12-04 16:39:28 +0300

    done task 1

--- HWLessen1/Program.cs
+++ HWLessen1/Program.cs
@@ -1,22 +1,25 @@
-﻿// Задача 2: Напишите программу, 
-//которая на вход принимает два числа и выдаёт, 
-//какое число большее, а какое меньшее.
+﻿//Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
 
-//a = 5; b = 7 -> max = 7
-//a = 2 b = 10 -> max = 10
-//a = -9 b = -3 -> max = -3
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
 
-Console.WriteLine("Введите первое число: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 числа:");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 = Convert.ToInt32(Console.ReadLine());
+int number_3 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число: ");
-int numberB = Convert.ToInt32(Console.ReadLine());
+int max = number_1;
 
-if (numberA > numberB)
-{       
- Console.WriteLine("Первое число " +  numberA +  " больше, чем второе " + numberB);
-}
-else
+if (number_2 > max)
 {
-Console.WriteLine("Второе число " + numberB + " больше, чем первое " + numberA);
+    max = number_2;
 }
+
+if (number_3 > max)
+{
+    max = number_3;
+}
+
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
