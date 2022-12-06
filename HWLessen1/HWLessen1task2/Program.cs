e91ee2ef3fb0bf330131a1df3e32039e332e0132
johndenisov <johndenisov@gmail.com>
2022-12-07 01:13:55 +0300

    done task 2

--- HWLessen1/HWLessen1task2/Program.cs
+++ HWLessen1/HWLessen1task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Введите 3 числа:");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 = Convert.ToInt32(Console.ReadLine());
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number_1;
+
+if (number_2 > max)
+{
+    max = number_2;
+}
+if (number_3 > max)
+{
+    max = number_3;
+}
+
+Console.WriteLine("Наибольшее число: " + max);
