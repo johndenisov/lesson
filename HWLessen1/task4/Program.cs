1327167c490cfafaad9ae1af571ad80d1af69a49
johndenisov <johndenisov@gmail.com>
2022-12-08 00:34:50 +0300

    done task4

--- HWLessen1/task4/Program.cs
+++ HWLessen1/task4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+Console.WriteLine("Чётные числа от 1 до " + number);
+while (i <= number)
+{
+    if (i % 2 != 1)
+        {
+        Console.Write(i + ", ");
+        }
+    i++;
+}
