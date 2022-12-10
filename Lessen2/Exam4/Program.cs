596a47261bd8826305facfa47496b51f7eccb5ab
johndenisov <johndenisov@gmail.com>
2022-12-10 20:07:44 +0300

    done Exam4 Array.Length

---
+++ Lessen2/Exam4/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {11, 18, 31, 41, 15, 61, 17, 18, 19};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+} 
+
+
