af31af38edec9bc89b87b15d8f56c1e8b46927e8
johndenisov <johndenisov@gmail.com>
2022-12-10 20:59:50 +0300

    done Exam5 void

---
+++ Lessen2/Exam5 void/Program.cs
@@ -0,0 +1,31 @@
+﻿void FillArray(int[] collection)
+{
+
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+
+        collection[index] = new Random().Next();
+        index++;
+    }
+    
+}
+
+void PrintArray(int[] col)
+
+
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+        
+    }
+}    
+int [] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
