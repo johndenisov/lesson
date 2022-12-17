8d492b9c6779e8479bead2c25f6c29371a3c5e36
johndenisov <johndenisov@gmail.com>
2022-12-18 01:14:36 +0300

    done task3

--- Lessen2/HWLessen2/task3/Program.cs
+++ Lessen2/HWLessen2/task3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 3: Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+//14 -> нет
+//46 -> нет
+//161 -> да
+
+Console.WriteLine("Введите число ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if ((Number % 7 == 0) && (Number % 23 == 0))
+    {
+       Console.WriteLine("Данное число:  " +  Number  + " одновременно кратно 7 и 23"); 
+    }
+else
+    {
+    Console.WriteLine("Данное число одновременно некратно 7 и 23 ");
+
+    }
