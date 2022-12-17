605694aec1ab99234d8b806cdc6665e31fa1fa58
johndenisov <johndenisov@gmail.com>
2022-12-17 21:37:55 +0300

    done Sem2 Exam2

---
+++ Lessen2/Sem2/Exam2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишие программу , которая принимает на вход два числа 
+///и сообщает является ли второе число кратным первому.
+// Если одно число кратно другому.
+
+Console.WriteLine("Ввведите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввведите число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    if(a % b == 0) //кратное
+    {
+        Console.WriteLine("Кратное ");
+    }
+    else
+    {
+        int result = a % b;
+        Console.WriteLine("Некратное остаток равен " +  result);
+    }
+}
+else
+{
+    if(b % a == 0)
+    {
+        Console.WriteLine("Кратное ");
+    }
+    else
+    {
+        int result = b % a;
+        Console.WriteLine("Некратное остаток равен " +  result);
+    }
+
+}
