d2e30f0e56facac8b03f68ec1379a1e0faf8b495
johndenisov <johndenisov@gmail.com>
2022-12-17 22:37:28 +0300

    done task1

--- Lessen2/HWLessen2/task1/Program.cs
+++ Lessen2/HWLessen2/task1/Program.cs
@@ -4,3 +4,18 @@
 //782 -> 8
 //918 -> 1
 
+Console.WriteLine("Введи трёхзначное число: ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+string Number = Convert.ToString(threeDigitNumber);
+
+if (Number.Length > 2)
+    {
+    Console.WriteLine("вторая цифра этого числа  " + Number[1]);
+    }
+else 
+    {
+    Console.WriteLine("Вы ввели не трехзначное число ");
+    }
+
