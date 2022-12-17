9a28d63b6238c67497574118aaeabdb91eb5e349
johndenisov <johndenisov@gmail.com>
2022-12-17 20:41:06 +0300

    start Sem2

---
+++ Lessen2/Sem2/Program.cs
@@ -0,0 +1,16 @@
+﻿//С# Генерация случайного числа с условиями диапазона.
+//получить 1 цифру
+// 2 цифру
+//Сравнение
+int N = new Random().Next(10, 99);
+int firstDigit = N / 10;
+int secondDigit = N % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
