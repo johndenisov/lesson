1ef2fca0f610ae37946e4e6205003db35f5abf1c
johndenisov <johndenisov@gmail.com>
2022-12-22 00:07:59 +0300

    done task1

--- Lessen3/HWLessen3/task1/Program.cs
+++ Lessen3/HWLessen3/task1/Program.cs
@@ -6,3 +6,22 @@
 12821 -> да
 23432 -> да */
 
+Console.WriteLine("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Введенное число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Введенное число: {number} - не палиндром.");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else 
+
+Console.WriteLine($"Введите правильное число");
